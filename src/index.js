89585b671f69386c183a02bc017b9abc296aa8c5
Rodrigo Hernando <rhernandog@gmail.com>
2020-04-14 13:52:39 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+	return <div className="container">
+		<div className="row">
+			<div className="col-12">
+				<h1 className="text-center">GSAP React Base App</h1>
+				<p className="lead">This features a series of answers for the React related questions in the GSAP Forums.</p>
+				<hr />
+			</div>
+		</div>
+	</div>
+}
+
+render(
+	<App />, document.getElementById("root")
+);
