90d2ea4406740c06773aecb1590f9fb167a485ee
Rodrigo Hernando <rhernandog@gmail.com>
2020-07-10 19:30:22 -0400

    Stop Wheel Update - Updated eslint file

--- src/components/WheelDynamicStop.js
+++ src/components/WheelDynamicStop.js
@@ -11,6 +11,7 @@ class WheelDynamicStop extends Component {
       stopSpinAngle: null,
       stopSpin: false
     };
+    this.startAngle = 0;
     this.spinTween = null;
     this.wheel = null;
     this.stopSpin = false;
@@ -20,41 +21,91 @@ class WheelDynamicStop extends Component {
   startSpinHandler = () => {
     this.setState({ stopSpin: false });
     this.loopIteration = 0;
+    this.createSpinAnimation();
     gsap.to(this.spinTween, {
       ease: "none",
       duration: 0.75,
       timeScale: 1
     });
-  };
+  }
 
   stopSpinHandler = () => {
     this.setState({
       stopSpin: true,
-      stopSpinAngle: 254
+      stopSpinAngle: 45
     });
-  };
+  }
 
-  componentDidMount() {
+  createSpinAnimation = () => {
+    if (this.wheel._gsap) {
+      // this.startAngle = this.wheel._gsap.get(this.wheel, "rotation");
+      console.log(parseFloat(this.wheel._gsap.get(this.wheel, "rotation").replace("deg", "")));
+      this.startAngle = parseFloat(this.wheel._gsap.get(this.wheel, "rotation").replace("deg", ""));
+    }
     this.spinTween = gsap.to(this.wheel, {
-      rotation: 360,
+      overwrite: "true",
+      rotation: 360 + this.startAngle,
+      repeat: -1,
+      duration: 0.875,
       ease: "none",
-      duration: .875,
       onRepeat: () => {
         const { stopSpin, stopSpinAngle } = this.state;
-        if (this.loopIteration >= 5 && stopSpin) {
+        if (this.loopIteration >= 2 && stopSpin) {
           gsap.to(this.spinTween, {
             ease: "power1.inOut",
-            duration: 2 + (360 / stopSpinAngle),
+            duration: 1.75 + (0.875 * stopSpinAngle / 360),
             timeScale: 0
           });
         }
         this.loopIteration++;
       },
-      repeat: -1,
-      paused: true
     })
       .timeScale(0)
-      .play();
+      .play(0);
+    //   this.spinTween = gsap.to(this.wheel, {
+    //     rotation: 360,
+    //     ease: "none",
+    //     duration: .875,
+    // onRepeat: () => {
+    //   const { stopSpin, stopSpinAngle } = this.state;
+    //   if (this.loopIteration >= 2 && stopSpin) {
+    //     gsap.to(this.spinTween, {
+    //       ease: "power1.inOut",
+    //       duration: 1.75 + (0.875 * stopSpinAngle / 360),
+    //       timeScale: 0
+    //     });
+    //   }
+    //   this.loopIteration++;
+    // },
+    //     repeat: -1,
+    //     paused: true
+    //   })
+    //     .timeScale(0)
+    //     .play();
+  }
+
+  componentDidMount() {
+    this.createSpinAnimation();
+    // this.spinTween = gsap.to(this.wheel, {
+    //   rotation: 360,
+    //   ease: "none",
+    //   duration: .875,
+    //   onRepeat: () => {
+    //     const { stopSpin, stopSpinAngle } = this.state;
+    //     if (this.loopIteration >= 2 && stopSpin) {
+    //       gsap.to(this.spinTween, {
+    //         ease: "power1.inOut",
+    //         duration: 1.75 + (0.875 * stopSpinAngle / 360),
+    //         timeScale: 0
+    //       });
+    //     }
+    //     this.loopIteration++;
+    //   },
+    //   repeat: -1,
+    //   paused: true
+    // })
+    //   .timeScale(0)
+    //   .play();
   }
 
   render() {
@@ -67,7 +118,9 @@ class WheelDynamicStop extends Component {
           <li>Stop at specific angle</li>
           <li>Spin a minimum amount of turns before stopping</li>
         </ol>
-        <div className="spinner" ref={e => this.wheel = e}></div>
+        <div className="spinner" ref={e => this.wheel = e}>
+          <div className="line"></div>
+        </div>
         <button onClick={this.startSpinHandler} className="btn btn-info" id="play">Play</button>
         <button onClick={this.stopSpinHandler} className="btn btn-info ml-3" id="stop">Stop at 254°</button>
       </div>
